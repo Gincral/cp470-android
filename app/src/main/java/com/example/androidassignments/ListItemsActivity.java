458f11f0d77d30120bfa4e9f0fa20ecd23a27ce3
Gincral <ssssrxdd@gmail.com>
2020-10-07 01:03:15 -0400

    few steps

--- app/src/main/java/com/example/androidassignments/ListItemsActivity.java
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.java
@@ -5,10 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ListItemsActivity extends AppCompatActivity {
+    
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        android.util.Log.i(ACTIVITY_NAME, "In onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+    }
+
+
+    @Override
+    protected void onStart() {
+        android.util.Log.i(ACTIVITY_NAME, "In onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        android.util.Log.i(ACTIVITY_NAME, "In onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        android.util.Log.i(ACTIVITY_NAME, "In onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        android.util.Log.i(ACTIVITY_NAME, "In onDestroy()");
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/androidassignments/MainActivity.java
+++ app/src/main/java/com/example/androidassignments/MainActivity.java
@@ -6,9 +6,37 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected static final String ACTIVITY_NAME = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        android.util.Log.i(ACTIVITY_NAME, "In onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onStart() {
+        android.util.Log.i(ACTIVITY_NAME, "In onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        android.util.Log.i(ACTIVITY_NAME, "In onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        android.util.Log.i(ACTIVITY_NAME, "In onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        android.util.Log.i(ACTIVITY_NAME, "In onDestroy()");
+        super.onDestroy();
     }
 }
